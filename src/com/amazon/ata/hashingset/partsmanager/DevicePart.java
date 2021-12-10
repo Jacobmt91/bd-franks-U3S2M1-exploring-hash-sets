73fa55acf7831557b3b091a27eef9f512afa4368
Jacob T <Jacob.m.thompson4@gmail.com>
2021-12-10 12:56:31 -0700

    Completed guided project

--- src/com/amazon/ata/hashingset/partsmanager/DevicePart.java
+++ src/com/amazon/ata/hashingset/partsmanager/DevicePart.java
@@ -1,11 +1,14 @@
 package com.amazon.ata.hashingset.partsmanager;
 
 import java.util.List;
+import java.util.Objects;
 
+// immutable - value can not be change once its assigned
+// mutable - value can be changed with setters
 public class DevicePart {
-    private String manufacturer;
-    private String manufacturersPartNumber;
-    private List<AmazonDevice> devicesUsedIn;
+    private String manufacturer;                // read-only immutable value
+    private String manufacturersPartNumber;     // read-only immutable value
+    private List<AmazonDevice> devicesUsedIn;   // read and write mutable value
 
     public DevicePart(String manufacturer, String manufacturersPartNumber, List<AmazonDevice> devicesUsedIn) {
         this.manufacturer = manufacturer;
@@ -41,5 +44,32 @@ public class DevicePart {
     public String toString() {
         return String.format("Device Part: {manufacturer: %s, manufacturersPartNumber: %s, devicesUsedIn: %s}",
                 manufacturer, manufacturersPartNumber, devicesUsedIn);
+    }
+
+    // @Override rules: same name, parameters, and return type
+    @Override
+    public boolean equals(Object o) { // Note: Object is used for the parameter type because the method we are overriding has Object class as a parameter type
+        // The next line is for training purposes only
+        System.out.println("DevicePart equals()...");
+        // if the object being compared is the same as the object your comparing to, they are equal
+        if (this == o) return true;
+
+        // if the object being compared is null or a different class than the object your comparing to, they are equal
+        if (o == null || getClass() != o.getClass()) return false;
+
+        // instantiating an object of a class used in our comparison (object passed is an Object class object)
+        DevicePart that = (DevicePart) o;
+
+        // Use the class data members to determine equality
+        // Note the use of the String class .equals to check manufacturer and manufacturerPartNumber
+        return getManufacturer().equals(that.getManufacturer()) && getManufacturersPartNumber().equals(that.getManufacturersPartNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        // The next line is for training purposes only
+        System.out.println("DevicePart hashCode()...");
+        // Use the Java hash() to generate a Hash Code using the immutable data member of the class
+        return Objects.hash(getManufacturer(), getManufacturersPartNumber());
     }
 }
--- src/com/amazon/ata/hashingset/partsmanager/PartManager.java
+++ src/com/amazon/ata/hashingset/partsmanager/PartManager.java
@@ -4,9 +4,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class PartManager {
-    private Set<DevicePart> deviceParts = new HashSet<>();
+    // instantiate a HashSet of DeviceParts to keep track of what parts we are currently using
+    // Hashset and HashCode should only be used on immutable data
+    // Set<DevicePart> is a base class or super case while HashSet<> is the subclass or child class (Inheritance is-a relationship)
+    private Set<DevicePart> deviceParts = new HashSet<>(); // Elements in a Hash must have a unique Hash Code
 
     public void addDevicePart(DevicePart devicePart) {
+        // Use the Hashset .add() to add the part passed to use to the deviceParts hashSet
+        //      .add() automatically calls the hashCode() method to determine the Hash Code
+        //      .add() sometimes also calls the equals() method when there is a Hash Code collision
         boolean isAdded = deviceParts.add(devicePart);
     }
 
